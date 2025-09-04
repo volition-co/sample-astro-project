fbd5fdcf5e0bf34c8a3aaa4794e8c7b4bbd22599
Arjun Singh <arjun810@gmail.com>
2025-09-03 23:49:06 -0700

    Add agent config.mjs

---
+++ astro.agent.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  host: true,
+  allowedHosts: ['AGENT_WEB_HOST_PLACEHOLDER'],
+});
