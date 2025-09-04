f37d9ca0dcf9191cde63bc4f934c5b1d3998fcb9
Arjun Singh <arjun810@gmail.com>
2025-09-04 00:01:55 -0700

    Fix server block for agent config

--- astro.agent.config.mjs
+++ astro.agent.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  host: true,
-  allowedHosts: ['AGENT_WEB_HOST_PLACEHOLDER'],
+  server: {
+    host: true,
+    allowedHosts: ['AGENT_WEB_HOST_PLACEHOLDER'],
+  }
 });
